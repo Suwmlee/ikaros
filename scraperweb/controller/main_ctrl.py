a80f494ed9256983c5dfaa8b06a479dd29d2f7d1
root <suwmlee@gmail.com>
2021-01-15 11:11:18 +0800

    Refine ctrl

--- scraperweb/controller/main_ctrl.py
+++ scraperweb/controller/main_ctrl.py
@@ -13,26 +13,17 @@ from ..service.info import infoService, transferService
 from ..service.setting import settingService
 from ..service.task import taskService
 from ..utils.wlogger import wlogger
-from concurrent.futures import ThreadPoolExecutor
+# from concurrent.futures import ThreadPoolExecutor
 
 # DOCS https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.ThreadPoolExecutor
-executor = ThreadPoolExecutor(2)
+# executor = ThreadPoolExecutor(2)
 
 
-@web.route("/api/start", methods=['POST'])
+@web.route("/api/scrape", methods=['POST'])
 def start_scraper():
     try:
         # executor.submit(manager.start)
         manager.start()
-        return Response(status=200)
-    except Exception as err:
-        wlogger.info(err)
-        return Response(status=500)
-
-
-@web.route("/api/startscan", methods=['POST'])
-def start_scan():
-    try:
         return Response(status=200)
     except Exception as err:
         wlogger.info(err)
@@ -95,6 +86,27 @@ def get_transfer(page):
         else:
             ret['running'] = False
         return json.dumps(ret)
+    except Exception as err:
+        wlogger.info(err)
+        return Response(status=500)
+
+
+@web.route("/api/setting", methods=['GET'])
+def getSetting():
+    try:
+        content = settingService.getSetting().serialize()
+        return json.dumps(content)
+    except Exception as err:
+        wlogger.info(err)
+        return Response(status=500)
+
+
+@web.route("/api/setting", methods=['POST'])
+def updateSetting():
+    try:
+        content = request.get_json()
+        settingService.updateSetting(content)
+        return Response(status=200)
     except Exception as err:
         wlogger.info(err)
         return Response(status=500)
