f23bf542c485c695ced2c58ab9b7148ba900373b
Mathhew <suwmlee@gmail.com>
2021-06-22 14:38:31 +0800

    Update number_parser.py

--- src/utils/number_parser.py
+++ src/utils/number_parser.py
@@ -1,7 +1,10 @@
 import os
 import re
-# from ..bizlogic import *
 
+
+G_spat = re.compile(
+    "(^22-sht\.me|-fhd|_fhd|^fhd_|^fhd-|-hd|_hd|^hd_|^hd-|-sd|_sd|-1080p|_1080p|-720p|_720p)",
+    re.IGNORECASE)
 
 
 def get_number(debug,filepath: str) -> str:
@@ -33,19 +36,28 @@ def get_number(debug,filepath: str) -> str:
     if debug == False:
         try:
             if '-' in filepath or '_' in filepath:  # 普通提取番号 主要处理包含减号-和_的番号
-                filepath = filepath.replace("_", "-")
-                filepath.strip('22-sht.me').strip('-HD').strip('-hd')
+                #filepath = filepath.replace("_", "-")
+                filepath = G_spat.sub("", filepath)
                 filename = str(re.sub("\[\d{4}-\d{1,2}-\d{1,2}\] - ", "", filepath))  # 去除文件名中时间
-                if 'FC2' or 'fc2' in filename:
-                    filename = filename.replace('PPV', '').replace('ppv', '').replace('--', '-').replace('_', '-')
-                file_number = re.search(r'\w+-\w+', filename, re.A).group()
+                lower_check = filename.lower()
+                if 'fc2' in lower_check:
+                    filename = lower_check.replace('ppv', '').replace('--', '-').replace('_', '-').upper()
+                file_number = re.search(r'\w+(-|_)\w+', filename, re.A).group()
+                if "tokyo" in lower_check and "hot" in lower_check:
+                    file_number = re.search(r'(cz|gedo|k|n|red-|se)\d{2,4}', lower_check, re.A).group()
+                elif "carib" in lower_check:
+                    file_number = str(re.search(r'\d{6}(-|_)\d{3}', lower_check, re.A).group()).replace('_', '-')
+                elif "1pon" in lower_check or "paco" in lower_check:
+                    file_number = str(re.search(r'\d{6}(-|_)\d{3}', lower_check, re.A).group()).replace('-', '_')
+                elif "10mu" in lower_check:
+                    file_number = str(re.search(r'\d{6}(-|_)\d{2}', lower_check, re.A).group()).replace('-', '_')
                 return file_number
             else:  # 提取不含减号-的番号，FANZA CID
                 # 欧美番号匹配规则
                 oumei = re.search(r'[a-zA-Z]+\.\d{2}\.\d{2}\.\d{2}', filepath)
                 if oumei:
                     return oumei.group()
-                
+
                 try:
                     return str(
                         re.findall(r'(.+?)\.',
@@ -58,19 +70,28 @@ def get_number(debug,filepath: str) -> str:
             return
     elif debug == True:
         if '-' in filepath or '_' in filepath:  # 普通提取番号 主要处理包含减号-和_的番号
-            filepath = filepath.replace("_", "-")
-            filepath.strip('22-sht.me').strip('-HD').strip('-hd')
+            #filepath = filepath.replace("_", "-")
+            filepath = G_spat.sub("", filepath)
             filename = str(re.sub("\[\d{4}-\d{1,2}-\d{1,2}\] - ", "", filepath))  # 去除文件名中时间
-            if 'FC2' or 'fc2' in filename:
-                filename = filename.replace('PPV', '').replace('ppv', '').replace('--', '-').replace('_', '-')
-            file_number = re.search(r'\w+-\w+', filename, re.A).group()
+            lower_check = filename.lower()
+            if 'fc2' in lower_check:
+                filename = lower_check.replace('ppv', '').replace('--', '-').replace('_', '-').upper()
+            file_number = re.search(r'\w+(-|_)\w+', filename, re.A).group()
+            if "tokyo" in lower_check and "hot" in lower_check:
+                file_number = re.search(r'(cz|gedo|k|n|red-|se)\d{2,4}', lower_check, re.A).group()
+            elif "carib" in lower_check:
+                file_number = str(re.search(r'\d{6}(-|_)\d{3}', lower_check, re.A).group()).replace('_', '-')
+            elif "1pon" in lower_check or "paco" in lower_check:
+                file_number = str(re.search(r'\d{6}(-|_)\d{3}', lower_check, re.A).group()).replace('-', '_')
+            elif "10mu" in lower_check:
+                file_number = str(re.search(r'\d{6}(-|_)\d{2}', lower_check, re.A).group()).replace('-', '_')
             return file_number
         else:  # 提取不含减号-的番号，FANZA CID
             # 欧美番号匹配规则
             oumei = re.search(r'[a-zA-Z]+\.\d{2}\.\d{2}\.\d{2}', filepath)
             if oumei:
                 return oumei.group()
-            
+
             try:
                 return str(
                     re.findall(r'(.+?)\.',
