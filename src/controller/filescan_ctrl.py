ae91ebd273c9bbe4a8c824d480d6f236b8426e70
Mathhew <suwmlee@gmail.com>
2021-09-09 15:40:19 +0800

    fix: scan file permission

--- src/controller/filescan_ctrl.py
+++ src/controller/filescan_ctrl.py
@@ -11,16 +11,23 @@ from . import web
 @web.route('/api/scan/', )
 @web.route('/api/scan/<path:media_dir>', )
 def direcotry(media_dir=''):
-    parentdir = os.path.dirname(media_dir)
-    ret = dict()
-    ret['parent'] = parentdir
-    dir_ele_list = list()
-    for f in (Path('/') / Path(media_dir)).iterdir():
-        fullname = str(f).replace('\\', '/')
-        if f.is_dir():
+    try:
+        parentdir = os.path.dirname(media_dir)
+        ret = dict()
+        ret['parent'] = parentdir
+        dir_ele_list = list()
+        for f in (Path('/') / Path(media_dir)).iterdir():
             fullname = str(f).replace('\\', '/')
-            dir_ele_list.append({'is_dir': 1, 'filesize': 0,
-                                 'url': url_for('web.direcotry', media_dir=fullname[0:]),
-                                 'fullname': fullname})
-    ret['dirs'] = dir_ele_list
-    return json.dumps(ret)
+            if f.is_dir():
+                fullname = str(f).replace('\\', '/')
+                dir_ele_list.append({'is_dir': 1, 'filesize': 0,
+                                    'url': url_for('web.direcotry', media_dir=fullname[0:]),
+                                    'fullname': fullname})
+        ret['dirs'] = dir_ele_list
+        return json.dumps(ret)
+    except PermissionError:
+        ret = {'error': '拒绝访问'}
+        return json.dumps(ret)
+    except Exception as e:
+        ret = {'error': str(e)}
+        return json.dumps(ret)
