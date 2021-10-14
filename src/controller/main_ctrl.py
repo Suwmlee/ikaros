4aed8beca7c67ba834dcc54af38753cdbc2cdcc7
Mathhew <suwmlee@gmail.com>
2021-10-14 15:37:05 +0800

    chore: update scripts

--- src/controller/main_ctrl.py
+++ src/controller/main_ctrl.py
@@ -88,7 +88,7 @@ def client_auto():
 
 #!/bin/bash
 TR_DOWNLOADS="$TR_TORRENT_DIR/$TR_TORRENT_NAME"
-curl -XPOST http://192.168.1.233:12346/api/client -H 'Content-Type: application/json' \
+curl -XPOST http://127.0.0.1:12346/api/client -H 'Content-Type: application/json' \
 --data @<(cat <<EOF
 {"path":"$TR_DOWNLOADS"}
 EOF
