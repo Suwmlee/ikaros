dcf6320d8deca702aaa05f862a9fc884dc57fbaa
Mathhew <suwmlee@gmail.com>
2021-12-30 23:11:46 +0800

    fix: add intro imgs route

--- src/controller/view_ctrl.py
+++ src/controller/view_ctrl.py
@@ -1,8 +1,18 @@
 
-from flask import render_template
+import os
+from flask import render_template, Response
 from . import web
 
 
 @web.route("/")
 def index():
     return render_template('index.html')
+
+
+@web.route("/imgs/<imageid>")
+def imgs(imageid):
+    localPath = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(localPath, "..", "..", "docs", "imgs", imageid), 'rb') as f:
+        image = f.read()
+    resp = Response(image, mimetype="image/jpeg")
+    return resp
