f77535e1290f4c6553ccf8e316e8974f199152fc
Mathhew <suwmlee@gmail.com>
2022-12-07 14:40:20 +0800

    feat: update lib api

--- src/controller/optionctrl.py
+++ src/controller/optionctrl.py
@@ -4,14 +4,13 @@ import os
 import json
 import logging
 import time
-
-from flask import request, Response
-
+import subprocess
+from flask import request, Response, current_app
 
 from . import web
-from flask import current_app
 from ..service.configservice import localConfService
 from ..bizlogic.schedulertask import cleanRecordsTask
+from ..utils.regex import regexMatch
 
 
 @web.route("/api/options/loglevel", methods=['GET', 'PUT'])
@@ -81,4 +80,32 @@ def getLocalConfig():
 def updateLocalConf():
     content = request.get_json()
     localConfService.updateConfig(content)
+    return Response(status=200)
+
+
+@web.route("/api/options/check/lib", methods=['GET'])
+def checklibversion():
+    """ 检测scrapinglib版本
+    """
+    info = subprocess.run("python -m pip index -vv versions scrapinglib", shell=True, stdout=subprocess.PIPE, encoding="utf8")
+    out = info.stdout
+    current_app.logger.debug(out)
+    install_matches = regexMatch(out, "INSTALLED:\ ([\d.]+)")
+    latest_matches = regexMatch(out, "LATEST:[\ ]+([\d.]+)")
+    installed = install_matches[0] if len(install_matches) == 1 else "Unknown"
+    latest = latest_matches[0] if len(latest_matches) == 1 else "Unknown"
+    ret = {
+        "name": "scrapinglib",
+        "installed": installed,
+        "latest": latest
+    }
+    return json.dumps(ret)
+
+
+@web.route("/api/options/check/lib", methods=['PUT'])
+def updatelibversion():
+    """ 更新scrapinglib
+    """
+    info = subprocess.run("python -m pip install scrapinglib -U", shell=True, stdout=subprocess.PIPE, encoding="utf8")
+    current_app.logger.debug(info.stdout)
     return Response(status=200)
