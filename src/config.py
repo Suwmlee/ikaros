a2455fe416551eac91199babf7a09b964f104c90
Mathhew <suwmlee@gmail.com>
2023-05-22 17:59:37 +0800

    chore: update log format

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ class Config:
     DEBUG = False
     SECRET_KEY = 'secret!'
     SCHEDULER_API_ENABLED = True
-    LOGGING_FORMAT = '[%(asctime)s] %(module)-15s %(levelname)s : %(message)s'
+    LOGGING_FORMAT = '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
     LOGGING_LOCATION = 'data/web.log'
     LOGGING_LEVEL = logging.INFO
     VERSION = '2.0.4'
