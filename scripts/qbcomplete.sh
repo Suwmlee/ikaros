84b6c0e06e6a2eb67605a5730a8325533244e982
zyuerugou <44331849+zyuerugou@users.noreply.github.com>
2023-11-12 00:35:54 +0800

    Update qbcomplete.sh
    如果docker的挂载路径与qbit挂载路径不同名的话，需要用以下命令a="%F"&& sh qbcomplete.sh ${a/qbit挂载路径/ikros挂载路径}

--- scripts/qbcomplete.sh
+++ scripts/qbcomplete.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # qbcomplete.sh "%F"
+# 如果docker的挂载路径与qbit挂载路径不同名的话，需要用以下命令a="%F"&& sh qbcomplete.sh ${a/qbit挂载路径/ikros挂载路径}
 
 QB_DOWNLOADS="${1}"
 curl -XPOST http://127.0.0.1:12346/api/client -H 'Content-Type: application/json' \
